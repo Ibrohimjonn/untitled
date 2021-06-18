3414cf9215c9fa377641a6cf7a24a10c60024135
Ibrohimjonn <gurilinfraligov@gmail.com>
2021-06-18 19:31:09 +0500

    hello

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,57 +13,66 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(245, 167, 188, 1),
+        title: Text('Taehyung and Jennie',style: TextStyle(color: Colors.white,fontSize: 20),),
+          centerTitle: true,
+        elevation: 0,
+      ),
       body: Container(
         color: Color.fromRGBO(245, 167, 188, 1),
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              height: 600,
+              width: double.infinity,
+              child: PageView(
+                physics: BouncingScrollPhysics(),
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset('assets/tae.jpg',fit: BoxFit.cover,),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset('assets/jennie.jpg',fit: BoxFit.cover,),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset('assets/tae.jpg',fit: BoxFit.cover,),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset('assets/jennie.jpg',fit: BoxFit.cover,),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            height: 500,
-            width: double.infinity,
-            child: PageView(
-              physics: BouncingScrollPhysics(),
-              children: [
-                 Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                   child: ClipRRect(
-                     borderRadius: BorderRadius.circular(16),
-                     child: Image.asset('assets/tae.jpg',fit: BoxFit.cover,),
-                   ),
-                  ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.asset('assets/jennie.jpg',fit: BoxFit.cover,),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.asset('assets/tae.jpg',fit: BoxFit.cover,),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.asset('assets/jennie.jpg',fit: BoxFit.cover,),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          ],
         ),
       ),
     );
